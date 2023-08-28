860e62feb9a3ffecce5a151d56cf4868e558bffa
Raphael Rivas <raphaelrivas@hotmail.com>
2023-08-28 09:21:18 -0300

    feat: convert images into existing sprite and frame

--- README.md
+++ README.md
@@ -2,9 +2,61 @@
 
 Convert images into sprites for Game Maker Studio
 
+## Requirements
+
+Mandatory:
+- Python 3.10.x
+
+Recommended
+- conda or pyenv etc
+
+## Running
+
+If you just want to run this program do the following:
+
+1. Install requirements:
+   
+   ```powershell
+   pip install -r requirements.txt
+   ```
+
+2. Run the program
+
+    ```powershell
+    python -u img2spr/img2spr.py update --images "<path>" --sprite "<path>"
+    ```
+
+## Development
+
+1. Create your env and install requirements:
+
+    ```powershell
+    conda create -n img2spr python=3.10
+    conda activate img2spr
+    pip install -r requirements.txt
+    ```
+
+2. Activate environment (recommended)
+
+   ```powershell
+   conda activate img2spr
+   ```
+
+3. Run the program:
+
+   ```powershell
+   python -y img2spr/img2spr.py
+   ```
+
 ## TODO
 
 v0.0.1
 - [x] Create basic project setup
-- [ ] Simple CLI interface
-- [ ] Use existing sprite with frames to copy images onto it
+- [x] Simple CLI interface
+- [x] Use existing sprite with existing frames to copy images onto it
+
+v0.0.2
+- [ ] Update required frames to match selected image
+
+v0.1.0
+- [ ] 
--- img2spr/img2spr.py
+++ img2spr/img2spr.py
@@ -1,53 +1,84 @@
 import argparse
 import os
 import json5
-
+import shutil
 
 class Img2spr:
     """Convert image to sprite"""
     settings: dict
     sprite: dict
+    image_extension: str = ".png"
 
     def set_settings(self, settings: dict):
         """Set settings"""
-        new_settings = settings
-        sprite_folder = new_settings.get("sprite_folder_destination")
+        image_folder = settings["new_images_folder_path"]
+        sprite_folder = settings["sprite_folder_path"]
         self.sprite = {}
         if(isinstance(sprite_folder, str)):
-            folder_name = sprite_folder.replace("\\", "/").split("/")[-1]
-            png_files = [file for file in os.listdir(sprite_folder) if file.endswith('.png')]
-            settings_file_path = f"{sprite_folder}/{folder_name}.yy"
+            sprite_name = sprite_folder.replace("\\", "/").split("/")[-1]
+            image_files = sorted([file for file in os.listdir(image_folder) if file.endswith('.png')])
+            settings_file_path = f"{sprite_folder}/{sprite_name}.yy"
             with open(settings_file_path, 'r', encoding='utf8') as file:
                 settings_data = json5.load(file)
             new_sprite = {
-                "folder_name": folder_name,
-                "png_files": png_files,
+                "sprite_name": sprite_name,
+                "new_image_files": image_files,
                 "settings": settings_data
             }
             self.sprite = new_sprite
-    
+        
+        self.settings = settings
 
+    def get_sorted_sprite_frames(self) -> list:
+        """Gest sorted list of sprite's frames"""
+        items = self.sprite["settings"]["sequence"]["tracks"][0]["keyframes"]["Keyframes"]
+        items_map = {
+            i["Channels"]["0"]["Id"]["name"]: i["Key"]
+            for i in  items
+        }
+        names = items_map.keys()
+        sorted_names = sorted(names, key=lambda name: items_map[name])
+        return sorted_names
+
+    def udpate_sprite(self):
+        """Update existing sprite and existing frames with new images"""
+        sprite_frame_images = self.get_sorted_sprite_frames()
+        for i, sprite_frame in enumerate(sprite_frame_images):
+            new_image_filepath = os.path.join(
+                self.settings["new_images_folder_path"],
+                self.sprite["new_image_files"][i],
+            )
+            layer = self.sprite["settings"]["layers"][0]["name"]
+            sprite_frame_path = os.path.join(
+                self.settings["sprite_folder_path"],
+                sprite_frame + self.image_extension
+            )
+            sprite_layer_path = os.path.join(
+                self.settings["sprite_folder_path"],
+                "layers", sprite_frame,
+                layer + self.image_extension
+            )
+            shutil.copy(new_image_filepath, sprite_frame_path)
+            shutil.copy(new_image_filepath, sprite_layer_path)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="App Description")
-    
     subparsers = parser.add_subparsers(dest="command")
-    
-    run_parser = subparsers.add_parser("convert", help="Run conversion")
-    run_parser.add_argument("--image", "-i", required=True, help="Image folder source path")
+
+    run_parser = subparsers.add_parser("update", help="Run conversion")
+    run_parser.add_argument("--images", "-i", required=True, help="Image folder source path")
     run_parser.add_argument("--sprite", "-s", required=True, help="Sprite folder destination path")
 
     args = parser.parse_args()
 
     img2spr = Img2spr()
 
-    if args.command == "convert":
+    if args.command == "update":
         img2spr.set_settings({
-            "image_folder_source": args.image_folder_source,
-            "sprite_folder_destination": args.sprite_folder_destination,
+            "new_images_folder_path": args.images,
+            "sprite_folder_path": args.sprite,
         })
-        # Your logic for the 'run' command here
-        print(f"settings: {img2spr.sprite}")
+        img2spr.udpate_sprite()
     else:
         parser.print_help()
